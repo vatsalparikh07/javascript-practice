38d9dbbb48a5def1567e12e1dbdce94699c8ae91
Vatsal Parikh <65659649+vatsalparikh07@users.noreply.github.com>
2021-05-21 04:15:15 +0530

    Added Comments

--- jQuery/Typing Game/script.js
+++ jQuery/Typing Game/script.js
@@ -33,35 +33,87 @@ $(document).keypress(function (e) {
 
 });
 
-var sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+/* Global Declarations */
+
+var sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate']; 
 
 var sentenceIndex = 0;
 var letterIndex = 0;
 var currentSentence = sentences[sentenceIndex];
 var currentLetter = currentSentence[letterIndex];
 
+const numberofWords = 54;
+var numberofMistakes = 0;
+
+let startTime = new Date().getTime();
+let endTime = 0;
+
 $('#sentence').text(currentSentence);
 $('#target-letter').text(currentLetter);
 
+/* Keypress function */
+
 $(document).keypress(function(e){
-    console.log(currentLetter);
-    if(sentenceIndex < sentences.length){
-        if(letterIndex < currentSentence.length){    
-            if(currentLetter == e.key){
+
+    if(sentenceIndex < sentences.length){       
+
+        if(letterIndex < currentSentence.length){   
+
+            /* Checks if character displayed is equal to the key pressed */
+            if(currentLetter == e.key){         
+                $('#feedback').append('<span class = \'glyphicon glyphicon-ok\'></span>');      
+
                 letterIndex++;
                 currentLetter = currentSentence[letterIndex];
+                
                 $('#target-letter').text(currentLetter);
                 $('#yellow-block').css('left','+=17.5px');
             }
+
+            else{
+
+                $('#feedback').append('<span class = \'glyphicon glyphicon-remove\'></span>');
+                numberofMistakes++;            
+
+            }
         }
+
+        /* Increments the sentence and resets other values */
+
         else{
+
             letterIndex = 0;
             sentenceIndex++;
             currentSentence = sentences[sentenceIndex];
             currentLetter = currentSentence[letterIndex];
+
             $('#sentence').text(currentSentence);
             $('#target-letter').text(currentLetter);
             $('#yellow-block').css('left','');
+            $('#feedback').empty();
+
         }
+    }
+
+    else if(sentenceIndex == 5){   
+
+        /* Calculates words per minute */
+        endTime = new Date().getTime();
+        let minutes = ((endTime - startTime)/(1000*60));
+        let WPM = Math.round((numberofWords/minutes) - (2*numberofMistakes));       
+
+        $('#sentence').css('text-align','center');
+        $('#sentence').text('Your words per minute score is: ' + WPM);
+        $('#feedback').empty();
+        $('#feedback').text('No. of mistakes you made were: ' + numberofMistakes);
+        $('#target-letter').empty();
+        $('#yellow-block').hide();
+        
+        setTimeout(function(){
+            if(confirm("Wanna Play Again?")){       
+                location.reload();
+            }
+        },2000); 
+
     }
 });
