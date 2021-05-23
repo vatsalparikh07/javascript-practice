be352f9c731a03331fc185c97379f9d28b9bbf8c
Vatsal Parikh <65659649+vatsalparikh07@users.noreply.github.com>
2021-05-24 01:02:51 +0530

    Added closure property to function

--- Context Clues/script.js
+++ Context Clues/script.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded',function (){
 
+    /* Object declarations in arrays */
+
     var friends = ['Arun','Beena','Chetan','Dhruv','Geeta'];
 
     var locationName = ['drawing room', 'dining room', 'kitchen' , 'balcony' , 'bedroom' , 
@@ -8,11 +10,24 @@ document.addEventListener('DOMContentLoaded',function (){
     var weapon = ['scissors','knife','sniper rifle','pistol','sword','nail cutter','rod','blade','bomb','axe',
                     'pen','pencil','water bottle','broom','wire','fork','steel ruler','cutter','tong','stone'];
 
-    for(var i = 1; i<101; i++){
+    for(var i = 0; i<100; i++){
+
+        /*Creates 100 h3 elements */
         var h3 = document.createElement('h3');
-        var h3Text = document.createTextNode('Accusation ' + i);
+        var h3Text = document.createTextNode('Accusation ' + (i+1) + "🔍");
         h3.appendChild(h3Text);
         document.body.appendChild(h3);
+
+        /* Calls the function Accusation on click */
+        h3.addEventListener('click',Accusation(i));
+        
+    }
+
+    /*Uses the closure property to create a private variable so that the count i doesn't start from 101 */
+    function Accusation(i){
+        return function alertAccusation(){
+            alert("Accusation " + (i+1) + ": I accuse " + friends[i%friends.length] + " with the " + weapon[i%weapon.length] + " in the " + locationName[i%locationName.length]);
+        }
     }
 
 });
